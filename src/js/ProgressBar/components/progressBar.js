e35209e211eaeea21525226f0752c68e1f3a095e
jknanda78 <j_k_nanda@hotmail.com>
2016-05-23 23:30:29 -0700

    adding progressbar css, component update

--- src/js/ProgressBar/components/progressBar.js
+++ src/js/ProgressBar/components/progressBar.js
@@ -5,20 +5,30 @@
  * @author Jyoti Kaustuv Nanda <j_k_nanda@hotmail.com>
  */
 
+require("../style/progressBar.scss");
 import React, {PropTypes} from "react";
 
-const {string, object, func, array, number} = PropTypes;
+const {array} = PropTypes;
 
 const ProgressBar = (props) => {
+  const {steps} = props;
+  const list = steps.map((item) => {
+    return (
+        <li id={item.name} key={item.name}>{item.label}</li>
+    );
+  });
+
   return (
-    <div>
-      <h4>Progress Bar</h4>
+    <div className="progress-bar">
+      <h2>Progress Bar</h2>
+      <ul>
+        {list}
+      </ul>
     </div>
   );
 };
 
 ProgressBar.propTypes = {
-  noOfSteps: number.isRequired,
   steps: array.isRequired
 };
 
